25922f2d508ac7545f81f40c12b9e276ee315e07
gowtham116helloworldgit <gowthambugappa18@gmail.com>
2024-12-08 10:15:26 +0000

    solution

---
+++ src/components/CowinDashboard/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+const CowinDashboard = () => {
+  return (
+    <div className="app-container">
+      <h1 className="landing-page">LANDING PAGE</h1>
+      <div className="buttons-container">
+        <button className="seemore-button">See More</button>
+        <button className="call-button">Call to Action</button>
+      </div>
+    </div>
+  )
+}
+export default CowinDashboard
